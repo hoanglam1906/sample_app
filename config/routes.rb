eb1602940be9cc748fc079419c6f3754005ea51f
Hoang Lam <hoanglam1906@gmail.com>
2018-07-12 11:21:42 +0700

    pull 5: chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/i18n"
-  root "static_pages#i18n"
+
+  root "static_pages#home"
+  get  "/home",    to: "static_pages#home"
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
 
   scope "(:locale)", locale: /en|vi/ do
-    root to: "static_pages#i18n"
+    root to: "static_pages#home"
   end
 end
